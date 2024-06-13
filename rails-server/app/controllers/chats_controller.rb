c6d232b23a97086746ee5bc39ac8c3f10df2ccda
loheesong <loheesong@gmail.com>
2024-06-13 20:07:14 +0800

    refactor

--- rails-server/app/controllers/chats_controller.rb
+++ rails-server/app/controllers/chats_controller.rb
@@ -22,34 +22,34 @@ class ChatsController < ApplicationController
         # display content on index.html
     end
 
-  def get_chat_with_id
+    def get_chat_with_id
         @user_1_chat_id = {} # just so application.html.erb doesnt cry 
         chat_id = params[:chat_id].to_i # use :chat_id as defined in config/routes.rb, not :id like in application.html.erb
         @chat_history = get_chatid_from_db(chat_id)
         render("index")
-  end 
+    end 
 
-  private # methods defined here onwards is private 
-  def get_chatid_from_db(find_chat_id)
-    # example content, this method will be replaced with method that calls python server 
-    all_chatids_with_content = [
-        {
-            :id => 1,
-            :content => "aaa"
-        },
-        {
-            :id => 2,
-            :content => "bbb"
-        },
-        {
-            :id => 3,
-            :content => "ccc"
-        },
-    ]
-    all_chatids_with_content.each do |chat_history|
-        if chat_history[:id] == find_chat_id
-            return chat_history
+    private # methods defined here onwards is private 
+    def get_chatid_from_db(find_chat_id)
+        # example content, this method will be replaced with method that calls python server 
+        all_chatids_with_content = [
+            {
+                :id => 1,
+                :content => "aaa"
+            },
+            {
+                :id => 2,
+                :content => "bbb"
+            },
+            {
+                :id => 3,
+                :content => "ccc"
+            },
+        ]
+        all_chatids_with_content.each do |chat_history|
+            if chat_history[:id] == find_chat_id
+                return chat_history
+            end
         end
-    end
-  end 
+    end 
 end

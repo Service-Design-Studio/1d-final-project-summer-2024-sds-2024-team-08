4643927a0959b4cff084975bac51264ab436e212
loheesong <loheesong@gmail.com>
2024-06-13 20:54:09 +0800

    feat: support chat history for diff uid

--- rails-server/app/controllers/chats_controller.rb
+++ rails-server/app/controllers/chats_controller.rb
@@ -2,54 +2,65 @@ class ChatsController < ApplicationController
     def index
         # "convention over configuration" -> do not explicit render something at the end of controller action 
         # Rails auto look for action_name.html.erb template 
-
-        # given a user id, get chat ids
-        @user_1_chat_id = [
-            {
-                :id => 1,
-                :title => "chat 1"
-            },
-            {
-                :id => 2,
-                :title => "chat 2"
-            },
-            {
-                :id => 3,
-                :title => "chat 3"
-            },
-        ]
-        # click on button on sidebar -> make get request for chat messages 
-        # display content on index.html
+        user = 1
+        @list_of_chat_ids = get_chatid_given_userid(user)
+        @chat_history = []
     end
 
     def get_chat_with_id
-        @user_1_chat_id = {} # just so application.html.erb doesnt cry 
+        @list_of_chat_ids = [] # just so application.html.erb doesnt cry TODO: fix this 
         chat_id = params[:chat_id].to_i # use :chat_id as defined in config/routes.rb, not :id like in application.html.erb
-        @chat_history = get_chatid_from_db(chat_id)
+        @chat_history = get_messages_given_chatid(chat_id)
         render("index")
     end 
 
     private # methods defined here onwards is private 
-    def get_chatid_from_db(find_chat_id)
-        # example content, this method will be replaced with method that calls python server 
-        all_chatids_with_content = [
+    def get_chatid_given_userid(user_id)
+        # temp method that emulates db call 
+        content = [
             {
-                :id => 1,
-                :content => "aaa"
+                "uid": 1,
+                "chat_ids": [1,4,5]
             },
             {
-                :id => 2,
-                :content => "bbb"
+                "uid": 2,
+                "chat_ids": [2,3]
+            }
+        ].detect{|user| user[:uid] == user_id}[:chat_ids]
+    end
+
+    def get_messages_given_chatid(chat_id)
+        # temp method that emulates db call 
+        content = [
+            {
+                "chat_id": 1,
+                "messages": [
+                    {"id": 999, "role": "user", "content": "some fat ass string"},
+                    {"id": 998, "role": "assistant", "content": "assistant dump truck"},
+                ]
             },
             {
-                :id => 3,
-                :content => "ccc"
+                "chat_id": 2,
+                "messages": [
+                    {"id": 997, "role": "user", "content": "help me"},
+                    {"id": 996, "role": "assistant", "content": "no"},
+                ]
             },
-        ]
-        all_chatids_with_content.each do |chat_history|
-            if chat_history[:id] == find_chat_id
-                return chat_history
-            end
-        end
+            {
+                "chat_id": 3,
+                "messages": [
+                    {"id": 995, "role": "user", "content": "rails suck"},
+                    {"id": 994, "role": "assistant", "content": "get railed"},
+                ]
+            },
+            {
+                "chat_id": 4,
+                "messages": []
+            },
+            {
+                "chat_id": 5,
+                "messages": []
+            }
+        ].detect{|chat| chat[:chat_id] == chat_id}[:messages]
     end 
 end

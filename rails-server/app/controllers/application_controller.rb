694f1b71eb915241b6c6ba148e38790cc162acb1
loheesong <loheesong@gmail.com>
2024-06-19 17:50:45 +0800

    wip: deployment

--- rails-server/app/controllers/application_controller.rb
+++ rails-server/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     #Simulate logged-in user, prevent user from accessing chats using ID
     before_action :simulate_login
-    USER = 1
+    USER = 2
 
     def simulate_login(user_id = nil)
         session[:user_id] = USER # Simulating a logged-in user with ID 1

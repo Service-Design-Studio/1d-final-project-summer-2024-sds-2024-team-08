e6e1489875510c405ae7079bf1fd3c641c186543
loheesong <loheesong@gmail.com>
2024-07-03 12:01:12 +0800

    feat: db now using postgres with activerecord

--- rails-server/app/controllers/chats_controller.rb
+++ rails-server/app/controllers/chats_controller.rb
@@ -6,10 +6,8 @@ class ChatsController < ApplicationController
     before_action :set_chat_ids
     before_action :authorize_chat_access, only: [:get_chat_with_id]
 
-    # define constants here 
-    PY_SERVER_URL = "https://python-server-vdvad4wjla-de.a.run.app"
     #shifted USER to application controller
-
+    
     def index # Rails auto look for action_name.html.erb template
         @chat_history = []
         @default_landing_page = true
@@ -38,33 +36,12 @@ class ChatsController < ApplicationController
     end
 
     def get_chatid_given_userid(user_id)
-        chat_endpoint = "/chats?uid=#{user_id}"
-        uri = URI.parse(PY_SERVER_URL + chat_endpoint)
-        response = Net::HTTP.get_response(uri)
-
-        if response.is_a?(Net::HTTPSuccess)
-            data = JSON.parse(response.body)
-        else
-            puts "Error: #{response.message}"
-            return [] 
-        end
-
-        data["chat_ids"].map {|e| {chat_id: e, chat_name: "chat id: #{e}"}}
+        res = Chat.get_chatid_given_userid(user_id)
+        res[:chat_ids].map {|e| {chat_id: e, chat_name: "chat id: #{e}"}}
     end
 
     def get_messages_given_chatid(chat_id)
-        message_endpoint = "/messages?chat_id=#{chat_id}"
-        uri = URI.parse(PY_SERVER_URL + message_endpoint)
-        response = Net::HTTP.get_response(uri)
-
-        if response.is_a?(Net::HTTPSuccess)
-            data = JSON.parse(response.body)
-            pp data
-        else
-            puts "Error: #{response.message}"
-            return []
-        end
-
-        data["messages"]
+        res = Message.get_messages_given_chatid(chat_id)
+        res.map{|e| {"id"=> e['message_id'], "role"=> e['role'], "content"=> e['content']}}
     end 
 end
---
+++ rails-server/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+    def self.get_all
+        all
+    end 
+
+    # returns hash in this format {"user_id"=>1, "name"=>"Genie"}
+    def self.get_user_by_id(id) 
+        (res = where(user_id: id).first).nil? ? {} : res.attributes
+    end
+
+    def insert_new_user(name)
+
+    end 
+end

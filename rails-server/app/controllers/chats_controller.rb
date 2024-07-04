783ed425bc4bf202aa4beab8c8f8ffcc6142935c
loheesong <loheesong@gmail.com>
2024-07-04 17:14:02 +0800

    feat: form submits message to langchain api

--- rails-server/app/controllers/chats_controller.rb
+++ rails-server/app/controllers/chats_controller.rb
@@ -2,6 +2,9 @@ require 'net/http' # import to make HTTP requests
 require 'uri'
 require 'json'
 
+# constants here 
+LANGCHAIN_API = "https://stakeholder-api-hafh6z44mq-de.a.run.app"
+
 class ChatsController < ApplicationController
     before_action :set_chat_ids
     before_action :authorize_chat_access, only: [:get_chat_with_id]
@@ -21,12 +24,29 @@ class ChatsController < ApplicationController
     end 
 
     def handle_user_msg
-        puts params[:message]
-        
+        # message must be a string, post() expects a string
+        message = {'message'=>params[:message]}.to_json.to_s
+        puts message
+
         # send form contents to python side 
+        langchain_endpoint = "/langchain/"
+        uri = URI.parse(LANGCHAIN_API + langchain_endpoint)
+        response = Net::HTTP.post(uri, message, {'content-type': 'application/json'})
+
+        if response.is_a?(Net::HTTPSuccess)
+            data = JSON.parse(response.body)
+        else
+            puts "Error: #{response.message}"
+        end
+        p data 
+
+        # package response
+        data = {"id"=>100, "role"=>"assistant", "content"=>data["responses"]}
 
         params[:message] = nil
-        get_chat_with_id # call this method to handle redirect to get_chat_with_id screen 
+        # get_chat_with_id # call this method to handle redirect to get_chat_with_id screen 
+        @chat_id = params[:chat_id].to_i # use :chat_id as defined in config/routes.rb, not :id like in application.html.erb
+        get_chat_with_id
         return 
     end 
 

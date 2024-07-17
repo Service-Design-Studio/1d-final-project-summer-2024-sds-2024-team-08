a5b7729deb97d7f24edb1ead2380be877c3e71f5
Cai Ying <76650051+lim-cy@users.noreply.github.com>
2024-07-17 15:53:57 +0800

    feat(ui): loading animation
    - Loading animation displayed when post request is made
    - Scrollbar in the chat window defaults to the bottom of the chat now

--- rails-server/app/javascript/custom.js
+++ rails-server/app/javascript/custom.js
@@ -1,13 +1,37 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    setTimeout(() => {
-      let alerts = document.querySelectorAll('.alert');
-      alerts.forEach((alert) => {
-        alert.classList.remove('show');
-        alert.classList.add('hide');
-      });
-    }, 1500); 
-    
-    document.getElementById('menu-toggle').addEventListener('click', function() {
-      document.getElementById('sidebar').classList.toggle('hidden');
+  setTimeout(() => {
+    let alerts = document.querySelectorAll('.alert');
+    alerts.forEach((alert) => {
+      alert.classList.remove('show');
+      alert.classList.add('hide');
     });
+  }, 1500);
+
+  document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('hidden');
+  });
+
+  const form = document.getElementById('message-form');
+  const actionButtons = document.getElementById('action-buttons');
+  if (form) {
+    const loadingAnimation = document.getElementById('loading-animation');
+    const inputField = document.getElementById('message-input');
+    const formElements = form.querySelectorAll('.input-group, .actions');
+    const allElements = [...formElements, actionButtons];
+
+    form.addEventListener('submit', function(event) {
+      if (inputField.value.trim() === '') {
+        event.preventDefault();  // Prevent form submission if input is empty
+      } else {
+        allElements.forEach(element => element.classList.add('hidden'));
+        loadingAnimation.classList.remove('d-none');
+      }
+    });
+  }
+
+  const chatHistory = document.getElementById('chat-history');
+  if (chatHistory) {
+    chatHistory.scrollTop = chatHistory.scrollHeight;
+  }
+  
 });

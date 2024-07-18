4ca33f1776ac5b3672816ba27b42003153b6255d
bannontan <bannontan2001@hotmail.com>
2024-07-18 17:10:02 +0800

    Updated tools to convert floats and strings to integers.

--- python-server/app/langc.py
+++ python-server/app/langc.py
@@ -41,7 +41,8 @@ with Session(stakeholder_engine) as session:
 
 @tool
 def read_stakeholders(stakeholder_id: int = None, name: str = None, summary: bool = True, headline: bool = True, photo: bool = True) -> bytes:
-    """Use this tool to read stakeholders from the database. You can filter by stakeholder_id or name. From the prompt, identify the stakeholder by their name or stakeholder_id. You can also specify whether to include the summary, headline, and photo.
+    """Use this tool to read stakeholders from the database. You can filter by stakeholder_id or name. From the prompt, identify the stakeholder by their name or stakeholder_id. 
+    If you are using stakeholder_id, ensure that it is an integer before you input it into the read_stakeholders tool. You can also specify whether to include the summary, headline, and photo.
     This tool is used to get summaries and information about stakeholders from the database.
 
     The result returned will be in JSON format.
@@ -56,6 +57,8 @@ def read_stakeholders(stakeholder_id: int = None, name: str = None, summary: boo
     Returns:
         str: _description_
     """
+    if stakeholder_id is not None:
+        stakeholder_id = int(float(stakeholder_id))
     with Session(stakeholder_engine) as session:
         stakeholders = crud.get_stakeholders(session, stakeholder_id, name, summary, headline, photo)
         
@@ -65,7 +68,7 @@ def read_stakeholders(stakeholder_id: int = None, name: str = None, summary: boo
 def get_name_matches(name: str) -> list:
     """Use this tool to get the best matches for a given name. This tool will return a list of up to 5 stakeholder_ids who have names that are the best matches with the given name. 
     If the output only contains one stakeholder_id, then that is the best match. Depending on the context, if the the user wants to know more about the stakeholder, use the tool read_stakeholders tool to get the information about the identified stakeholder.
-    If the user wants to draw a network graph, use the tool get_relationships_with_names to get the relationships of the identified stakeholder.
+    If the user wants to draw a network graph, use the tool get_relationships to get the relationships of the identified stakeholder.
     If the output contains more than one stakeholder_id, then you should ask the user to clarify which stakeholder they are referring to. Following the format :
         "Which names are you referring to?: 
         1. [name 1]
@@ -77,7 +80,7 @@ def get_name_matches(name: str) -> list:
         name (str): The name of the stakeholder you want to find matches for.
         
     Returns:
-        list: A list of up to 5 stakeholder_id who have names that are the best matches with the given name.
+        list: A list of up to 5 stakeholder_id who have names that are the best matches with the given name. These stakeolder_id will be integers.
     """
     normalized_input_name = normalize_name(name)
     
@@ -105,8 +108,8 @@ def get_name_matches(name: str) -> list:
 @tool 
 def get_relationships_with_names(subject_id:int = None) -> bytes:
     '''
-    Use this tool to get the every relationship the stakeholders have with one another. This tool will return in JSON format, a list where each element is a list. The format is as such: '[[subject, predicate, object], [subject, predicate, object], ...]'. Where the subject is related to object by the predicate and the subject can have multiple relationships with objects.
-    After you have the relationships, you can use the tool generate_network to generate a network graph. The network graph will be generated and stored in the database. Once the graph has been stored, you will need to return the network_graph_id. This id will be used to retrieve the graph from the database.
+    Use this tool to output to the user every relationship the stakeholders have with one another in natural language. This tool should be called when the user wants the relationships of stakeholders in natural language. 
+    This tool will return in JSON format, a list where each element is a list. The format is as such: '[[subject, predicate, object], [subject, predicate, object], ...]'. Where the subject is related to object by the predicate and the subject can have multiple relationships with objects.
     If the output is an empty list then it means that the subject has no relationships with the object.
 
     Args:
@@ -115,6 +118,7 @@ def get_relationships_with_names(subject_id:int = None) -> bytes:
     Returns:
         list: A list of subjects, predicates and objects
     '''
+    subject_id = int(float(subject_id))
     with Session(stakeholder_engine) as session:
         subject_rs = crud.get_relationships_with_names(session, subject=subject_id)
         object_rs = crud.get_relationships_with_names(session, object=subject_id)
@@ -129,19 +133,62 @@ def get_relationships_with_names(subject_id:int = None) -> bytes:
         
     return subject_rs + object_rs
 
-def get_photo(name: str) -> str:
+@tool 
+def get_relationships(subject_id:int = None) -> bytes:
+    '''
+    Use this tool to get the every relationship the stakeholders have with one another. This tool will be used only if the user wants a network graph. This tool will return in JSON format, a list where each element is a list. The format is as such: '[[subject, predicate, object], [subject, predicate, object], ...]'. Where the subject is related to object by the predicate and the subject can have multiple relationships with objects.
+    After you have the relationships, you can use the tool generate_network to generate a network graph. The network graph will be generated and stored in the database. Once the graph has been stored, you will need to return the network_graph_id. This id will be used to retrieve the graph from the database.
+    If the output is an empty list then it means that the subject has no relationships with the object.
+
+    Args:
+        subject_id (int): The name of the stakeholder you want to find matches for.
+        
+    Returns:
+        relationships_with_predicates (list[list[int, str, int]]): A list of subjects, predicates and objects. 
+            The subject is the stakeholder_id of the subject and it is an integer. 
+            The predicate is the relationship between the subject and the object. The predicate is a string. 
+            The object is the stakeholder_id of the object and it is an integer.
+    '''
+    subject_id = int(float(subject_id))
     with Session(stakeholder_engine) as session:
-        response = session.scalars(select(Stakeholder).where(Stakeholder.name == name).limit(1)).one_or_none()
+        subject_rs = crud.get_relationships(session, subject=subject_id)
+        object_rs = crud.get_relationships(session, object=subject_id)
+    
+    if subject_rs == None and object_rs == None:
+        return []
+    
+    elif subject_rs == None:
+        relationships = object_rs
+    
+    elif object_rs == None:
+        relationships = subject_rs
+    
+    else:    
+        relationships = subject_rs + object_rs
+    
+    relationships_with_predicates = []
+    for result in relationships:
+        predicate = result.predicate
+        extracted_info = crud.extract_after_last_slash(predicate)
+        extracted_info = re.sub(r'[^a-zA-Z0-9\' ]', '', extracted_info)
+        relationships_with_predicates.append((result.subject, extracted_info, result.object))
+        
+    return relationships_with_predicates
+
+def get_photo(stakeholder_id: int) -> str:
+    stakeholder_id = int(stakeholder_id)
+    with Session(stakeholder_engine) as session:
+        response = session.scalars(select(Stakeholder).where(Stakeholder.stakeholder_id == stakeholder_id).limit(1)).one_or_none()
         if response is not None:
             ref_pic = response.photo
             if ref_pic:
                 pic_url = ref_pic.split("||")[0].strip()  # Split by "||" and take the first URL
                 return pic_url
             else:
-                print(f"No photo field for get_photo({name}): {response}")
+                print(f"No photo field for get_photo({stakeholder_id}): {response}")
                 return None
         else:
-            print(f"stakeholder {name} does not exist")
+            print(f"stakeholder {stakeholder_id} does not exist")
             return None
 
 g_id = {}  # Dictionary to store the generated network graph id a global dictionary
@@ -152,12 +199,13 @@ def generate_tools(chat_id: int) -> List[BaseTool]:
     def generate_network(relationships: list[list[str]]) -> dict:
         """You need to consider the predicates in each of the relationships and identify the appropriate ones only based on the user input. If the user did not mention any specific context, then you can consider all the relationships. 
         You can use the tool map_data to generate the network graph. The network graph will be generated and stored in the database. Once the graph has been stored, you will need to return the network_graph_id. This id will be used to retrieve the graph from the database.
+        This tool will be called when the user mentions that they want a visualisation or if they want to draw a network graph or simply a graph.
         
         Args:
             relationships (list): A list where each element is a list. The format is as such: '[[subject, predicate, object], [subject, predicate, object], ...]'. Where the subject is related to object by the predicate and the subject can have multiple relationships with objects.
             
         Returns:
-            results (str): A JSON object containing the network_graph_id.
+            results (str): A JSON object containing the message 'network graph has been created.'
         """
         subj_color="#77E4C8"
         obj_color="#3DC2EC"
@@ -170,17 +218,20 @@ def generate_tools(chat_id: int) -> List[BaseTool]:
         if buttons == True:
             g.width = "75%"
             g.show_buttons(filter_=["edges", "physics"])
-            
-        for rs in relationships:
-            subj = rs[0]
-            pred = rs[1]
-            obj = rs[2]
-            s_pic = get_photo(subj)
-            o_pic = get_photo(obj)
-            g.add_node(subj, color=subj_color, shape=subj_shape, image=s_pic)
-            g.add_node(obj, color=obj_color, shape=obj_shape, image=o_pic)
-            g.add_edge(subj,obj,label=pred, color=edge_color, smooth=False)
         
+        with Session(stakeholder_engine) as db:
+            for rs in relationships:
+                subject_id = int(float(rs[0]))
+                subj = crud.get_stakeholder_name(db, subject_id)
+                pred = rs[1]
+                object_id = int(float(rs[2]))
+                obj = crud.get_stakeholder_name(db, object_id)
+                s_pic = get_photo(subject_id)
+                o_pic = get_photo(object_id)
+                g.add_node(subj, color=subj_color, shape=subj_shape, image=s_pic)
+                g.add_node(obj, color=obj_color, shape=obj_shape, image=o_pic)
+                g.add_edge(subj,obj,label=pred, color=edge_color, smooth=False)
+            
         g.barnes_hut()
         g.toggle_physics(False)
         g.set_edge_smooth("dynamic")
@@ -195,10 +246,8 @@ def generate_tools(chat_id: int) -> List[BaseTool]:
             session.commit()
             generated_id = graph.id
         g_id[chat_id] = generated_id
-        print(f"generated id: {generated_id}")
-        print(f"g_id: {g_id}")
         
-        return {"network_graph_id": generated_id}
+        return {"message": "Network graph has been created!"}
     
     return [generate_network]
   
@@ -219,7 +268,7 @@ def query_model(query:str, user_id:int, chat_id:int) -> str:
 
     model = ChatVertexAI(model="gemini-1.5-flash", max_retries=2)
     
-    ls = [read_stakeholders, get_name_matches, get_relationships_with_names]
+    ls = [read_stakeholders, get_name_matches, get_relationships, get_relationships_with_names]
     
     tools = ls + generate_tools(chat_id=chat_id)
 

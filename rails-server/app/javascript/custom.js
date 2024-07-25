0319bdc8449f201b72aa3399bafe8edbbde95cf8
Cai Ying <76650051+lim-cy@users.noreply.github.com>
2024-07-25 15:47:00 +0800

    feat(ui): collapsible sidebar
    - Sidebar can now be toggled

--- rails-server/app/javascript/custom.js
+++ rails-server/app/javascript/custom.js
@@ -35,4 +35,24 @@ document.addEventListener('DOMContentLoaded', (event) => {
             iframe.classList.add('iframe-loaded');
         });
       });
+
+      document.getElementById('hamburger-menu').addEventListener('click', function () {
+        var sidebar = document.getElementById('sidebar');
+        var mainContent = document.getElementById('main-content');
+        var bottomSection = document.getElementById('bottom-section');
+
+        sidebar.classList.toggle('collapsed-sidebar');
+
+        if (sidebar.classList.contains('collapsed-sidebar')) {
+            sidebar.style.position = 'relative';
+            mainContent.classList.replace('col-12', 'col-9');
+            mainContent.classList.replace('col-lg-12', 'col-lg-10');
+            bottomSection.style.maxWidth = 'none';
+        } else {
+            sidebar.style.position = 'fixed';
+            mainContent.classList.replace('col-9', 'col-12');
+            mainContent.classList.replace('col-lg-10', 'col-lg-12');
+            bottomSection.style.maxWidth = '75vw';
+        }
+    });
 });

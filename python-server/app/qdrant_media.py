6de2b6070da38809eee0aec286903620cd7ccfa4
bannontan <bannontan2001@hotmail.com>
2024-08-06 10:39:32 +0800

    Broke up the derive_rs_from_media function into smaller components

--- python-server/app/qdrant_media.py
+++ python-server/app/qdrant_media.py
@@ -40,6 +40,8 @@ def derive_rs_from_media(model, stakeholder_id, query: str=""):
       media_ids = [hit.id for hit in hits]
     
     articles = get_content_from_media_ids(db, media_ids)
+    response = '\n'.join(articles)
+    return response
 
   # # llm_transformer = LLMGraphTransformer(llm=model)
   # documents = [Document(page_content='\n'.join(articles))]
@@ -48,27 +50,9 @@ def derive_rs_from_media(model, stakeholder_id, query: str=""):
   graph_documents = llm_transformer_custom(model, media_text='\n'.join(articles), user_query=query)
   
   return graph_documents
-  # nodes = graph_documents[0].nodes
-  # rs = graph_documents[0].relationships
-
-  # nodes_map = {node.id: i for i, node in enumerate(nodes)}
-
-  # media_rs = []
-  # for relation in rs:
-  #     source_id = nodes_map.get(relation.source.id)
-  #     target_id = nodes_map.get(relation.target.id)
-
-  #     if source_id and target_id:
-  #         media_rs.append([source_id, relation.type, target_id])
-  #     else:
-  #         # Either source_id or target_id does not appear in the nodes
-  #         continue
-      
-  # return {'nodes': {i: name for name, i in nodes_map.items()}, 'edges': media_rs, 'type': 'media'}
-  # return filtered_media_ids
 
 if __name__ == "__main__":
-  model = ChatVertexAI(model="gemini-1.5-flash", max_retries=2)
+  # model = ChatVertexAI(model="gemini-1.5-flash", max_retries=2)
   query = "Joe Biden supporters"
-  ls = derive_rs_from_media(model, stakeholder_id=28235, query=query)
+  ls = derive_rs_from_media(stakeholder_id=28235, query=query)
   print(ls)
